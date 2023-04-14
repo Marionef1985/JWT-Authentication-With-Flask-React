8a49be9aa647fce3bf611c6ce5d637c4c4859072
Marione Fiszman <marionef1985@gmail.com>
2023-04-14 10:33:24 +0000

    signup route

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,11 +8,17 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def create_new_user():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    is_active = request.json.get('is_active', None)
 
+    user = User(email=email, password=password, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "all ok"
     }
 
     return jsonify(response_body), 200
